2d4b85e9be8a624e0f637d8611c6d5bb96a586c9
vishnuaxobis <vishnu@axobis.com>
2026-02-24 14:50:12 +0530

    ip ip

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -310,7 +310,7 @@ const Attendance = require("../models/Attendance");
 const Employee = require("../models/Employee");
 
 // Company WiFi IP
-const COMPANY_IP_PREFIX = "14:39:44.75";
+const COMPANY_IP_PREFIX = "14:39:45.25";
 
 // Office GPS
 const OFFICE_LAT = 11.747809;
